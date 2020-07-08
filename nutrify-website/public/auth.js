32dce6914490b906b27d94f6e4b84a5dc49b4a40
Dr AlienTech <alientechgamig@hotmail.com>
2020-07-08 17:07:46 -0400

    Gitacora saved 1 changed files

--- nutrify-website/public/auth.js
+++ nutrify-website/public/auth.js
@@ -185,7 +185,7 @@ function getUserInput() {
 
     var age = inputText("age");
     var gender = inputText("gender");
-    var gender = inputText("gender");
+    var weight = inputText("gender");
     var healthIssues = inputText("healthIssues").replace(/ /g, ",").replace(/,,/g, ",").split(","); // comma or space separated values
     var allergies = inputText("allergies").replace(/ /g, ",").replace(/,,/g, ",").split(","); // comma or space separated values
     var name = inputText("name");
