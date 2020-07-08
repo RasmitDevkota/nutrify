750654404889757bfda704af5420c6f1a49dfab1
Dr AlienTech <alientechgamig@hotmail.com>
2020-07-07 23:42:05 -0400

    Gitacora saved 1 changed files

--- nutrify-website/public/auth.js
+++ nutrify-website/public/auth.js
@@ -175,7 +175,8 @@ function handleSignUp() {
 // User Health Survey Start
 function getUserInput() {
     if (!user) {
-        return console.log("no user found for user health survey")
+        return console.log("No user found for user health survey");
+        
     }
 
     function inputText(id) {
