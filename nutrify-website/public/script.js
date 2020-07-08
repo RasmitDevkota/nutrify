73c8917d87015c35d26b4e9696a949f51fcd0cf2
Dr AlienTech <alientechgamig@hotmail.com>
2020-07-07 20:11:37 -0400

    Gitacora saved 1 changed files

--- nutrify-website/public/script.js
+++ nutrify-website/public/script.js
@@ -35,11 +35,6 @@ firebase.auth().onAuthStateChanged(function (user) {
 });
 
 function pageLoad(u) {
-    if (window.location.href.includes("index.html") || !window.location.href.includes(".html")) {
-        xhttp("indexNavbar", "navbarHeader");
-    } else {
-        xhttp("navbar", "navbarHeader");
-    }
 
     xhttp("auth", "authDiv");
     xhttp("footer", "footerFooter");
